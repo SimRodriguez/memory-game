f23ad695575497c9ec3aa1760ccc2d86b0e119c0
S Rodriguez <simonne.rodriguez@gmail.com>
2024-06-10 17:39:30 -0400

    First commit, added base HTML, CSS, and the start of my Javascript

---
+++ app.js
@@ -0,0 +1,21 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const container = document.querySelector(".container");
+const cards = document.querySelector(".cards");
+const resetButton = document.querySelector("#reset")
+
+/*-------------------------------- Variables --------------------------------*/
+
+let cardChoices = [];
+let matchCounter = 0;
+let cardArr = Array.from(cards);
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
