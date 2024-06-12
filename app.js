880884af49b6fed70fcb54a5637933ae85d81413
S Rodriguez <simonne.rodriguez@gmail.com>
2024-06-12 12:28:33 -0400

    added click function and reset function

--- app.js
+++ app.js
@@ -1,78 +1,85 @@
 /*-------------------------------- Constants --------------------------------*/
-
 const cards = document.getElementsByClassName("cards-container");
-const restart = document.getElementById('restart');
-const imgsLinksArray = [ {
-    id: 'card1',
-    img: 'assets/avocado.png'
-    altName: 'Avocado image'
+const reset = document.getElementById('reset');
+const imgsLinksArray = [{
+	id: 'card1',
+	img: 'assets/avocado.png'
 }, {
-    id: 'card2',
-    img: 'assets/kiwi.png'
-    altName: 'Kiwi Image'
+	id: 'card2',
+	img: 'assets/kiwi.png'
 }, {
-    id: 'card3',
-    img: 'assets/orange.png'
-    altName: 'Orange Image'
+	id: 'card3',
+	img: 'assets/orange.png'
 }, {
-    id: 'card4',
-    img: 'assets/papaya.png'
-    altName: 'Papaya Image'
+	id: 'card4',
+	img: 'assets/papaya.png'
 }, {
-    id: 'card5', 
-    img: 'assets/strawberry.png'
-    altName: 'Strawberry Image'
+	id: 'card5',
+	img: 'assets/strawberry.png'
 }, {
-    id: 'card6',
-    img: 'assets/avocado.png'
-    altName: 'Avocado image'
+	id: 'card6',
+	img: 'assets/avocado.png'
 }, {
-    id: 'card7',
-    img: 'assets/kiwi.png'
-    altName: 'Kiwi Image'
+	id: 'card7',
+	img: 'assets/kiwi.png'
 }, {
-    id: 'card8',
-    img: 'assets/orange.png'
-    altName: 'Orange Image'
+	id: 'card8',
+	img: 'assets/orange.png'
 }, {
-    id: 'card9',
-    img: 'assets/papaya.png'
-    altName: 'Papaya Image'
+	id: 'card9',
+	img: 'assets/papaya.png'
 }, {
-    id: 'card10', 
-    img: 'assets/strawberry.png'
-    altName: 'Strawberry Image'
+	id: 'card10',
+	img: 'assets/strawberry.png'
 }]
-
 /*-------------------------------- Variables --------------------------------*/
-
 let allCardFaces = document.getElementsByClassName('card-face')
 let toggledCardsArray = [];
-
+let matchedCardsCount = 0;
+let messsage = document.getElementById('message')
 /*------------------------ Cached Element References ------------------------*/
-
-
-
 /*----------------------------- Event Listeners -----------------------------*/
+reset.addEventListener('click', init);
 
-
+for (let i = 0; i < cards.length; i++) {
+	cards[i].addEventListener('click', handleClick);
+}
 /*-------------------------------- Functions --------------------------------*/
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) { 
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]]; 
-}
-return array;
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
 }
 
 function init() {
-    const shuffledArray = shuffleArray(imgsLinksArray);
-    for (let i = 0; i < cards.length; i++) {
-    const cardFace = cards[i].querySelector('.card-face img');
-    cardFace.src = shuffleArray[i].img;
-    cardFace.alt = shuffleArray[i].altName;
-    cards[i].setAttribute('data-card-id', shuffleArray[i].id)
-    }
+	const shuffledArray = shuffleArray(imgsLinksArray);
+	for (let i = 0; i < cards.length; i++) {
+		const cardFace = cards[i].querySelector('.card-face img');
+		cardFace.src = shuffleArray[i].img;
+		cardFace.alt = shuffleArray[i].altName;
+		cards[i].setAttribute('data-card-id', shuffleArray[i].id)
+	}
 }
 
-init();
+function handleClick() {
+	this.classList.add("flipped");
+	toggledCardsArray.push(this);
+	if (toggledCardsArray.length === 2) {
+		const card1Id = toggledCardsArray[0].getAttribute('data-card-id');
+		const card2Id = toggledCardsArray[1].getAttribute('data-card-id');
+		if (card1id !== card2Id) {
+			setTimeout(() => {
+				toggledCardsArray.forEach(card => card.classList.remove("flipped"));
+				toggledCardsArray = [];
+			}, 1000);
+		} else {
+			toggledCardsArray = [];
+			matchedCardsCount++;
+			if (matchedCardsCount === cards.length / 2) {
+				messsage.textContent = " You won! Congratulations"
+			}
+		}
+	}
+}
