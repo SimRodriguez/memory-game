a8052f1e788bb566d58dae70a5c3cde5cf897f7b
S Rodriguez <simonne.rodriguez@gmail.com>
2024-06-12 10:33:57 -0400

    added id's to the cards to be able to call on them in javascript, additionally added effect toggles to cards in CSS for javascript to work with. Re-declared my constants and variables after having re organized and re-named my HTML

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const container = document.querySelector(".container");
-const cards = document.querySelector(".cards");
-const resetButton = document.querySelector("#reset")
+const cards = document.getElementsByClassName(".cards-container");
+const restart = document.getElementById('restart');
+const imgsLinksArray = [ {
+    id: card1,
+    img: assets/avocado.png
+    altName: 'Avocado image'
+}, {
+    id: card2,
+    img: assets/kiwi.png
+    altName: 'Kiwi Image'
+}, {
+    id: card3,
+    img: assets/orange.png
+    altName: 'Orange Image'
+}, {
+    id: card4,
+    img: assets/papaya.png
+    altName: 'Papaya Image'
+}, {
+    id: card5, 
+    img: assets/strawberry.png
+    altName: 'Strawberry Image'
+}, {
+    id: card6,
+    img: assets/avocado.png
+    altName: 'Avocado image'
+}, {
+    id: card7,
+    img: assets/kiwi.png
+    altName: 'Kiwi Image'
+}, {
+    id: card8,
+    img: assets/orange.png
+    altName: 'Orange Image'
+}, {
+    id: card9,
+    img: assets/papaya.png
+    altName: 'Papaya Image'
+}, {
+    id: card10, 
+    img: assets/strawberry.png
+    altName: 'Strawberry Image'
+}]
 
 /*-------------------------------- Variables --------------------------------*/
 
-let flippedCards = [];
-let locked = false;
+let allCardFaces = document.getElementsByClassName('card-face')
+let toggledCardsArray = [];
 
 /*------------------------ Cached Element References ------------------------*/
 
